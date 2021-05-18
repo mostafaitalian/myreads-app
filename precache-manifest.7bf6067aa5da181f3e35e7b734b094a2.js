9e26728b1009954226acb67d28e6f54c553005c3
mostafaitalian <eng_mustafa_yossef@hotmail.com>
2021-05-19 00:45:23 +0200

    Updates

---
+++ precache-manifest.7bf6067aa5da181f3e35e7b734b094a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "69890444e2a3b0dac6f7",
+    "url": "/github.io/myreads-app/static/css/main.e7847654.chunk.css"
+  },
+  {
+    "revision": "69890444e2a3b0dac6f7",
+    "url": "/github.io/myreads-app/static/js/main.69890444.chunk.js"
+  },
+  {
+    "revision": "8d1a3a5d68c5c499d3f4",
+    "url": "/github.io/myreads-app/static/js/1.8d1a3a5d.chunk.js"
+  },
+  {
+    "revision": "c2ce0ecfceb6d3317be1",
+    "url": "/github.io/myreads-app/static/js/runtime~main.c2ce0ecf.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/github.io/myreads-app/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/github.io/myreads-app/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/github.io/myreads-app/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "3fa5bccccf367054e00c62a6bfab1a0f",
+    "url": "/github.io/myreads-app/index.html"
+  }
+];
